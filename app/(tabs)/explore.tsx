cb6f5c174bbfdecc7f23f9f966ae125db38a89cc
Kendaka <barengkendrick4@gmail.com>
2025-10-07 23:00:03 +0400

    made to do on greeting

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,11 +1,11 @@
 import { Image } from 'expo-image';
 import { Platform, StyleSheet } from 'react-native';
 
-import { Collapsible } from '@/components/ui/collapsible';
 import { ExternalLink } from '@/components/external-link';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
+import { Collapsible } from '@/components/ui/collapsible';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Fonts } from '@/constants/theme';
 
@@ -21,6 +21,7 @@ export default function TabTwoScreen() {
           style={styles.headerImage}
         />
       }>
+        // TODO: Update greeting
       <ThemedView style={styles.titleContainer}>
         <ThemedText
           type="title"
