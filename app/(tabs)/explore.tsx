7f659af8500a1dabae8e4df8f82edc7d475b52f8
Kendaka <barengkendrick4@gmail.com>
2025-10-09 21:23:37 +0400

    set up navs

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -39,7 +39,7 @@ export default function TabTwoScreen() {
         </ThemedText>
         <ThemedText>
           The layout  in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the 
+          ses up the 
         </ThemedText>
         <ExternalLink href="https://docs.expo.dev/router/introduction">
           <ThemedText type="link"> </ThemedText>
