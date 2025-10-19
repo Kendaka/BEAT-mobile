2e73ecffd6379b631a207b6c62ac8bd878843759
Kendaka <barengkendrick4@gmail.com>
2025-10-19 21:57:19 +0400

    added helper join and two functions

---
+++ App.ts
@@ -0,0 +1,8 @@
+const join = {
+    greet: function(name: string): string {
+        return `Hello, ${name}!`;
+    },
+    farewell: function(name: string): string {
+        return `Goodbye, ${name}!`;
+    }
+};
