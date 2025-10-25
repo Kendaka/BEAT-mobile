e6604875d6f735e9469b96a2b263f8b5795dc21e
Kendaka <barengkendrick4@gmail.com>
2025-10-25 22:00:49 +0400

    smart helmet system

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>  </Text>
-      <Text style={styles.subtitle}>It's a smart helmet system by the way! Hopepfully I can finish this until the end.!</Text>
+      <Text style={styles.subtitle}>It's smart helmet system by the way! Hopepfully I can finish this until the end.!</Text>
     </View>
   );
 }
