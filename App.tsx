121746dd6fba2883c5d595357ab88b6f6836ecd2
Kendaka <barengkendrick4@gmail.com>
2025-10-26 17:59:07 +0400

    environmental on stylesheet, text

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}> BEAT - Bio </Text>
+      <Text style={styles.title}> BEAT - Bio Environmental</Text>
       <Text style={styles.subtitle}>It's smart helmet system by the way! Hopepfully I can finish this until the end.!</Text>
     </View>
   );
