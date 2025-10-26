cd57927a876fdd5127d6dc7c9da5d508ef4e4b6a
Kendaka <barengkendrick4@gmail.com>
2025-10-26 17:59:51 +0400

    centered draft styles and layout

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}> BEAT - Bio Environmental Alert</Text>
+      <Text style={styles.title}> BEAT - Bio Environmental Alert Technology</Text>
       <Text style={styles.subtitle}>It's smart helmet system by the way! Hopepfully I can finish this until the end.!</Text>
     </View>
   );
