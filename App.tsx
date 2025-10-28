b76a0cab11a88732ee0f6164fd5dae8eea034c24
Kendaka <barengkendrick4@gmail.com>
2025-10-28 21:21:17 +0400

    bgColor added

--- App.tsx
+++ App.tsx
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: '',
+    alignItems: 'center',
     backgroundColor: '#121212',
   },
   title: {
