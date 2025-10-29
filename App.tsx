0a279394bd9d0f64d8de2f07c4bc1bd91e4330ae
Kendaka <barengkendrick4@gmail.com>
2025-10-29 21:52:26 +0400

    removed container

--- App.tsx
+++ App.tsx
@@ -11,12 +11,7 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#121212',
-  },
+
   title: {
     color: '#00bfff',
     fontSize: 22,
