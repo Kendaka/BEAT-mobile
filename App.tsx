e860289a47efdc7970d2ea76737dc2580002b36d
Kendaka <barengkendrick4@gmail.com>
2025-10-29 21:53:36 +0400

    packaged changed

--- App.tsx
+++ App.tsx
@@ -11,7 +11,12 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#121212',
+  },
   title: {
     color: '#00bfff',
     fontSize: 22,
