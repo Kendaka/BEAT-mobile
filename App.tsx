5238766cc1efa804e41830702ba84b7b0cef086b
Kendaka <barengkendrick4@gmail.com>
2025-10-29 21:53:58 +0400

    descript changed

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}> BEAT - Bio Environmental Alert Technology</Text>
-      <Text style={styles.subtitle}>It's smart helmet system by the way! Hopepfully I can finish this until the end.!</Text>
+      <Text style={styles.subtitle}>It's intellect helmet system by the way! Hopepfully I can finish this until the end.!</Text>
     </View>
   );
 }
