1293cf5056cbd41fee2b188329bab4ce255728ab
Kendaka <barengkendrick4@gmail.com>
2025-11-02 18:35:23 +0400

    added style

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}> BEAT - Bio Env Alert Technology</Text>
-      <Text style={styles.subtitle}>It' by ! Hopepfully I can finish this until the end.!</Text>
+      <Text style={styles.subtitle}>It' Hopepfully I can finish this until the end.!</Text>
     </View>
   );
 }
