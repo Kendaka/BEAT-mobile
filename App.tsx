2a4dd2cc4f2023d992d3ef4160399c0527cfe945
Kendaka <barengkendrick4@gmail.com>
2025-11-03 21:07:39 +0400

    justfied content

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}> BEAT -</Text>
+      <Text style={styles.title}>  -</Text>
       <Text style={styles.subtitle}></Text>
     </View>
   );
