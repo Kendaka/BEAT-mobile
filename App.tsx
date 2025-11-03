3afa86932b669bd11b96cdea8cc1eb97f6c588a5
Kendaka <barengkendrick4@gmail.com>
2025-11-03 21:01:46 +0400

    stylesheet created

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}> BEAT - Bio Env Alert Technology</Text>
-      <Text style={styles.subtitle}>Hopepfully I can  </Text>
+      <Text style={styles.subtitle}>Hopepfully I </Text>
     </View>
   );
 }
