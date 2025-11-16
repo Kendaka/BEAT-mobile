3fd6e3434a752c17de9c9e3e1c14523701884d42
Kendaka <barengkendrick4@gmail.com>
2025-11-16 20:41:17 +0400

    added safety guidelines

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
+
 export default function App() {
   return (
     <View> 
       <Text style={styles.title}>GEAT System</Text>
-
+       <Text style={styles.subtitle}>Bio-Environmental Safety On The Go</Text>
     </View>
   );
 }
