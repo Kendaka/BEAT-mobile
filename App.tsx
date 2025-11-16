f88dc7a16bcffc35a4af6d4bd73325d5ade78ea9
Kendaka <barengkendrick4@gmail.com>
2025-11-16 20:42:16 +0400

    added styles and titles

--- App.tsx
+++ App.tsx
@@ -11,3 +11,21 @@ export default function App() {
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f2f4f7',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '700',
+  },
+  subtitle: {
+    fontSize: 14,
+    marginTop: 6,
+    color: '#666',
+  },
+});
+
