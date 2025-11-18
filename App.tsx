c43a9b9b14ea87335154032b368d41b3684610e1
Kendaka <barengkendrick4@gmail.com>
2025-11-18 21:10:37 +0400

    changed to f4f7

--- App.tsx
+++ App.tsx
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f2f4f7',
+    backgroundColor: '#f4f7',
   },
   title: {
     fontSize: 17,
