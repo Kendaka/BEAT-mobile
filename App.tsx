8cdda8a19e377d7469787dfbb99c1d3dcee0d820
Kendaka <barengkendrick4@gmail.com>
2025-11-29 21:30:53 +0400

    align item to center

--- App.tsx
+++ App.tsx
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 2,
-    alignItems: 'middle',
+    alignItems: 'center',
     backgroundColor: '#72727',
   },
   title: {
